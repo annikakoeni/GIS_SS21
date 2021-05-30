de11cca6facb3db880502fe9773328eb474f73b8
annikakoeni <80926929+annikakoeni@users.noreply.github.com>
2021-05-30 19:57:53 +0200

    3.1

--- Aufgabe3.1/serverscript.js
+++ Aufgabe3.1/serverscript.js
@@ -1,31 +1,30 @@
-import * as Http from "http";
-
-export namespace P_3_1Server {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.P_3_1Server = void 0;
+const Http = require("http");
+var P_3_1Server;
+(function (P_3_1Server) {
     console.log("Starting server");
-    let port: number = Number(process.env.PORT);
+    let port = Number(process.env.PORT);
     if (!port)
         port = 8100;
-        /**
-         * falls port nicht gleich 8100-> setze port auf 8100
-         */
-
-    let server: Http.Server = Http.createServer();
+    /**
+     * falls port nicht gleich 8100-> setze port auf 8100
+     */
+    let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
     /**
      * server initialisieren und listener hinzufügen
      */
-
-    function handleListen(): void {
+    function handleListen() {
         console.log("Listening");
     }
     /**
      * führt listener aus (Konsoloenausgabe listening)
      */
-
-
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    function handleRequest(_request, _response) {
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Zugriffsrechte
@@ -33,4 +32,5 @@ export namespace P_3_1Server {
         _response.end();
     }
     //** führt anfrage aus, gibt ausgaben aus, setzt html werte fest */
-}
+})(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
+//# sourceMappingURL=serverscript.js.map
