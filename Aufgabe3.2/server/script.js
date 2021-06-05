97c99a281e72ff3efa526e24f26de4b64ff8090f
annikakoeni <80926929+annikakoeni@users.noreply.github.com>
2021-06-05 16:43:02 +0200

    3.2

---
+++ Aufgabe3.2/server/script.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.P_3_2Server = void 0;
+const Http = require("http");
+const Url = require("url");
+var P_3_2Server;
+(function (P_3_2Server) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT); //create Port = "Gateway" to server
+    if (!port)
+        port = 8100; //set port to 8100 (if it wasn't before) 
+    let server = Http.createServer(); //create server
+    server.addListener("request", handleRequest); //Listener for Requests and Listen functions
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
+    }
+    function handleRequest(_request, _response) {
+        console.log(_request.url);
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            let path = url.pathname;
+            if (path == "/html") {
+                for (let key in url.query) {
+                    _response.write(key + ":" + url.query[key]);
+                }
+            }
+            if (path == "/json") {
+                let sentObject = JSON.stringify(url.query);
+                console.log(sentObject);
+                _response.write(sentObject);
+            }
+        }
+        _response.end();
+    }
+})(P_3_2Server = exports.P_3_2Server || (exports.P_3_2Server = {}));
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe3.2/server/script.ts
@@ -0,0 +1,46 @@
+import * as Http from "http";
+import * as Url from "url";
+
+export namespace P_3_2Server {
+    console.log("Starting server");
+    let port: number = Number(process.env.PORT);        //create Port = "Gateway" to server
+    if (!port)
+        port = 8100;                                    //set port to 8100 (if it wasn't before) 
+
+    let server: Http.Server = Http.createServer();      //create server
+    server.addListener("request", handleRequest);       //Listener for Requests and Listen functions
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    function handleListen(): void {                     //function Listen with console output
+        console.log("Listening");
+    }
+
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {      //function Request granting access for everyone and giving back the sent message url
+        console.log(_request.url);
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");  
+        
+        if (_request.url) {
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            let path: string = <string>url.pathname; 
+            if (path == "/html") {
+                for (let key in url.query) {
+                    _response.write(key + ":" + url.query[key]);
+                }
+            }
+            if (path == "/json") {
+                let sentObject: string = JSON.stringify(url.query);
+                console.log(sentObject);
+                _response.write(sentObject);
+                
+            }
+        }
+        _response.end();
+    }
+
+
+}
+    
+
